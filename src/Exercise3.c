a9e5fc4405fae0422a0bdcb4d6825f95c55c1f93
PhuongUyenITITIU20348 <ititiu20348@student.hcmiu.edu.vn>
2021-11-22 15:21:27 +0700

    ex2

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -16,6 +16,44 @@ Ex:
 void Ex3(char *str){
 	//Your codes here
 	
+    char input[100] = "";
+    char word[20] = "";
+    char longest[20] = "";
+    char shortest[20] = "";
+
+    int wordindex = 0;
+    printf ("Input a string: ");
+    fgets(input, sizeof(input), stdin);
+
+    for (int inputindex = 0; inputindex < strlen(input); inputindex++){
+        while (inputindex < strlen(input)&& !isspace(input[inputindex])&& isalnum(input[inputindex])) {  
+                    word[wordindex++]=input[inputindex++];
+                }
+                if (wordindex != 0)
+                {
+                    word[wordindex] = '\0';
+
+                    if (strlen(longest) == 0)
+                    {
+                        strcpy(longest, word);
+                    }
+                    if (strlen(shortest) == 0)
+                    {
+                        strcpy(shortest, word);
+                    }
+                    if (strlen(word) > strlen(longest))
+                    {
+                        strcpy(longest, word);
+                    }
+                    if (strlen(word) < strlen(shortest))
+                    {
+                        strcpy(shortest, word);
+                    }
+                    wordindex = 0;
+                }
+    }
+    printf ("Longest word: '%s'", longest);
+    printf ("\nShortest word: '%s'", shortest);
 }
 
 int main(int argc, char *argv[]) {
